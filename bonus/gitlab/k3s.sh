c91166dabeb09e15754400b51af63e902917032c
sabrina <sabrina@example.com>
2022-01-29 02:26:31 +0100

    wa akhiran chi 7aja khdama

---
+++ bonus/gitlab/k3s.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo \# updating apt repos
+apt update && apt upgrade -y &>/dev/null;
+
+echo \# installing k3s
+curl -sfL https://get.k3s.io | sh -;
+echo alias k=kubectl >> ~/.bashrc
+echo export KUBECONFIG=/etc/rancher/k3s/k3s.yaml >> ~/.bashrc
+
